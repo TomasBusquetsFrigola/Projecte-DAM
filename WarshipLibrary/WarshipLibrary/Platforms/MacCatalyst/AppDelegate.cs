ff1d6d755c831efc0d7a470a05ebde0ddd8a0d8d
TomasBusquetsFrigola <a2409805@institutmontilivi.cat>
2026-03-18 15:14:08 +0100

    Inici del projecte

---
+++ WarshipLibrary/WarshipLibrary/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WarshipLibrary
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
