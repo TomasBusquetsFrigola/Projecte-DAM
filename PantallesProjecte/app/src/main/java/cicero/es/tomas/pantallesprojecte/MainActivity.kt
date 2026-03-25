de535727ec34835f398d4fac28bc1079cbf0064c
TomasBusquetsFrigola <a2409805@institutmontilivi.cat>
2026-03-25 16:34:39 +0100

    Disseny de la pantalla principal iniciat

--- PantallesProjecte/app/src/main/java/cicero/es/tomas/pantallesprojecte/MainActivity.kt
+++ PantallesProjecte/app/src/main/java/cicero/es/tomas/pantallesprojecte/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import cicero.es.tomas.pantallesprojecte.ui.Screens.MainScreen
 import cicero.es.tomas.pantallesprojecte.ui.theme.PantallesProjecteTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             PantallesProjecteTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TODO("$innerPadding")
+//                    TODO("$innerPadding")
+                    MainScreen(innerPadding)
                 }
             }
         }
