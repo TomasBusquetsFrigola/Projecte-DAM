fa28664bf2b6eb79a71af1709977fe4a624c7f16
Tomàs <a2409805@institutmontilivi.cat>
2026-04-09 16:32:09 +0200

    Reiniciada general

--- APP Casino/composeApp/src/jvmMain/kotlin/com/tomasbusfri/files/appcasino/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.tomasbusfri.files.appcasino
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- CasinoPark/composeApp/src/jvmMain/kotlin/com/tomasbusfri/files/casinopark/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.tomasbusfri.files.casinopark
-
-class JVMPlatform {
-    val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-fun getPlatform() = JVMPlatform()
